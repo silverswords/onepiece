c8bb6e7e77f19e886cacdecc680882c3a327e818
Zhangbokai614 <64344118+Zhangbokai614@users.noreply.github.com>
2021-08-08 12:11:33 +0800

    chore(trending.js): get url and star and fork

--- cmd/trending.js
+++ cmd/trending.js
@@ -15,7 +15,7 @@ puppeteer.launch({ headless: true }).then(async browser => {
 
   await page.goto('https://github.com/trending/go?since=daily')
 
-  const name = await page.$$eval(".Box-row > .lh-condensed > a", item => item.map(e => e.text))
+  const nameAndUrl = await page.$$eval(".Box-row > h1 > a", item => item.map(e => {return {name: e.text, href: e.href}}))
   const overview = await page.$$eval(".Box-row > p", item => item.map(e => e.innerHTML))
   const cumulativeStar = await page.$$eval(".Box-row > .f6 > span + a", item => item.map(e => e.text))
   const fork = await page.$$eval(".Box-row > .f6 > span + a + a", item => item.map(e => e.text))
@@ -25,7 +25,8 @@ puppeteer.launch({ headless: true }).then(async browser => {
 
   for(let i = 0; i < 25; i++) {
     repositoriesList.push({
-      "name": name[i].replace(/[ ]|[\r\n]/g,""),
+      "name": nameAndUrl[i].name.replace(/[ ]|[\r\n]/g,""),
+      "url": nameAndUrl[i].href.replace(/[ ]|[\r\n]/g,""),
       "overview": overview[i].replace(/<[^>]+>|[\r\n]/g,"").trim(),
       "cumulativeStar": +cumulativeStar[i].replace(/[^\d.]/g,""),
       "star": +star[i].replace(/<[^>]+>|[\r\n]|[^\d.]/g,""),
